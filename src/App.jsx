62ac8a86d977116fbdb5028fdfcc8dc410d084e7
kabaiyaseng <kabaiyaseng@gmail.com>
2025-07-14 22:25:11 +0700

    hi

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// App.jsx
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./assets/pages/home";
+import Donate from "./assets/pages/donate";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/Donate">Donate</Link>
+          <Link to="/">Home</Link>
+          <Link to="/Donate">Donate</Link>
+          <Link to="/">Home</Link>
+          <Link to="/Donate">Donate</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Donate" element={<Donate />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
